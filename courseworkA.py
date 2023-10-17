29ec1d318ab5e53f95040207aad16d67e9dbeccd
George Madeley <george.madeley@outlook.com>
2023-10-17 12:16:52 +0100

    Added Support for Biases in Backpropagation

--- courseworkA.py
+++ courseworkA.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.special
+import matplotlib.pyplot as plt
 
 class NeuralNetwork:
     def __init__(self, numInputNodes, numOutputNodes, learningRate, *numHiddenNodesPerLayer):
@@ -51,10 +52,14 @@ class NeuralNetwork:
             activationInputs.append(np.dot(self.weights[layerIdx], activationOutputs[-1]))
             activationOutputs.append(self.activation(activationInputs[-1]))
             if layerIdx < self.numLayers - 1:
-                activationOutputs[-1] = np.append(activationOutputs[-1], [1])
+                activationOutputs[-1] = np.append(activationOutputs[-1], [[1]], axis=0)
+        # print("Activation Inputs:\n", activationInputs)
+        # print("Activation Outputs:\n", activationOutputs)
 
         errors = self.calculateErrors(targets, activationOutputs)
+        # print("Errors:\n", errors)
         self.updateWeights(activationOutputs, errors)
+        # print("------------------------------------------")
 
     def calculateErrors(self, targets, activationOutputs):
         errors = [0 for layer in range(self.numLayers)]
@@ -64,14 +69,23 @@ class NeuralNetwork:
         return errors
 
     def updateWeights(self, activationOutputs, errors):
-        for weightIdx in range(1, self.numLayers + 1):
-            layerError = errors[-weightIdx]
-            activationOutput = activationOutputs[-weightIdx]
-            prevActivationOutput = activationOutputs[-(weightIdx + 1)]
-            self.weights[weightIdx - 1] += self.learningRate * np.dot(
-                (layerError * activationOutput * (1.0 - activationOutput)),
-                np.transpose(prevActivationOutput)
-            )
+        for layerIdx in range(1, self.numLayers + 1):
+            # print(f"WEIGHTS:\n{self.weights}")
+            layerError = errors[-layerIdx]
+            activationOutput = activationOutputs[-layerIdx]
+            prevActivationOutput = activationOutputs[-(layerIdx + 1)]
+            # print(f"Layer Error:\t {layerError.shape}\n{layerError}")
+            # print(f"Activation Output:\t {activationOutput.shape}\n{activationOutput}")
+            # print(f"Prev Activation Output T:\t {prevActivationOutput.T.shape}\n{prevActivationOutput.T}")
+            product = (layerError * activationOutput * (1.0 - activationOutput))
+            if layerIdx > 1:
+                product = product[:-1]
+            # print(f"Product:\t{product.shape}\n{product}")
+            # print(f"Weight:\n{self.weights[-layerIdx]}")
+            dotProduct = (prevActivationOutput @ product.T).T
+            # print(f"dotProduct:\n{dotProduct}")
+            self.weights[-layerIdx] += self.learningRate * dotProduct
+            # print("==========================================")
 
     def query(self, inputs):
         inputs = np.append(np.array(inputs, ndmin=2), [[1]], axis=1).T
@@ -79,7 +93,10 @@ class NeuralNetwork:
         activationInputs = [None]
         activationOutputs = [inputs]
         for layerIdx in range(self.numLayers):
-            activationInputs.append(np.dot(self.weights[layerIdx], activationOutputs[-1]))
+            activationInput = np.dot(self.weights[layerIdx], activationOutputs[-1])
+            if len(activationInput.shape) != 2:
+                activationInput = np.reshape(activationInput, (activationInput.shape[0], 1))
+            activationInputs.append(activationInput)
             activationOutputs.append(self.activation(activationInputs[-1]))
             if layerIdx < self.numLayers - 1:
                 activationOutputs[-1] = np.append(activationOutputs[-1], [1])
@@ -112,15 +129,43 @@ class NeuralNetwork:
             print("Inputs:\t", inputs[index])
             print("Outputs:\t", outputs[index])
 
+    def showStateSpaceRepresentation(self, inputs, targets):
+        for layerIdx in range(self.numLayers):
+            if layerIdx == 0:
+                if self.numInputNodes == 3:
+                    bias = self.weights[0][0][-1]
+                    weights = self.weights[0][0][:-1]
+                    print(bias)
+                    print(weights)
+                    seperatorLineX = np.linspace(-1, 2, 100)
+                    seperatorLineY = (-weights[0]/weights[1])*seperatorLineX-(bias/weights[1])
+                    plt.plot(seperatorLineX, seperatorLineY)
+
+        for inputPair, target in zip(inputs, targets):
+            color = "red" if target == 0 else "green"
+            plt.scatter(inputPair[0], inputPair[1], s=50, color=color, zorder=3)
+
+        plt.xlim(-0.5, 1.5)
+        plt.ylim(-0.5, 1.5)
+
+        plt.xlabel("Input A")
+        plt.ylabel("Input B")
+        plt.title("State Space of Input Vector")
+
+        plt.grid(True, linewidth=1, linestyle=":")
+        plt.tight_layout()
+        plt.show()
+
         
 
     
-neuralNetwork = NeuralNetwork(2, 1, 0.1)
+neuralNetwork = NeuralNetwork(2, 1, 0.1, 2)
 inputs = [
   [0,0],
   [0,1],
   [1,0],
   [1,1]
 ]
-targets = [0, 0, 0, 1]
-neuralNetwork.trainUntilPass(inputs, targets, maxIterations=100000, minPrecision=0.1)
+targets = [0, 1, 1, 1]
+neuralNetwork.trainUntilPass(inputs, targets, maxIterations=10000, minPrecision=0.1)
+neuralNetwork.showStateSpaceRepresentation(inputs, targets)
