2a2528654c95d0827bdd1e2b82a852fbc5468c9b
George Madeley <george.madeley@outlook.com>
2023-10-23 11:31:26 +0100

    Improved Backpropagation Algorithm

--- courseworkA.py
+++ courseworkA.py
@@ -4,34 +4,38 @@ import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import progressbar
+import csv
 
-df_test_data = pd.read_csv("./MNIST/mnist_test.csv", header=None)
+sourceFileName = "./fashion_MNIST/fashion_mnist_"
+destinationFileName = "./results_fashion_2.csv"
+
+df_test_data = pd.read_csv(f"{sourceFileName}test.csv", header=None)
 test_labels = np.array(df_test_data[0])
-test_data = np.array(df_test_data[range(1, df_test_data.shape[1])], dtype=float) / 255
+test_data = (np.asfarray(df_test_data[range(1, df_test_data.shape[1])]) / 255 * 0.99) + 0.01
 numTestRows, numTestCols = test_data.shape
 
-df_train_data = pd.read_csv("./MNIST/mnist_train.csv", header=None)
+df_train_data = pd.read_csv(f"{sourceFileName}train.csv", header=None)
 train_labels = np.array(df_train_data[0])
 train_data = (
-    np.array(df_train_data[range(1, df_train_data.shape[1])], dtype=float) / 255
+    (np.asfarray(df_train_data[range(1, df_train_data.shape[1])]) / 255 * 0.99) + 0.01
 )
 numTrainRows, numTrainCols = train_data.shape
 
 numUniqueOutputs = len(df_train_data[0].unique())
 
 print("train data")
-y_value = np.zeros((1, 10))
+y_value = np.zeros((1, numUniqueOutputs))
 for i in range(numUniqueOutputs):
     print("occurance of ", i, "=", np.count_nonzero(train_labels == i))
     y_value[0, i - 1] = np.count_nonzero(train_labels == i)
 
 # converting train_label in one hot encoder representation
-train_labels_OHE = np.zeros((numTrainRows, numUniqueOutputs))
+train_labels_OHE = np.zeros((numTrainRows, numUniqueOutputs)) + 0.01
 for rowIdx in range(numTrainRows):
     label = train_labels[rowIdx]
     for colIdx in range(numUniqueOutputs):
         if label == colIdx:
-            train_labels_OHE[rowIdx, label] = 1
+            train_labels_OHE[rowIdx, label] = 0.99
 print("train_data shape=" + str(np.shape(train_data)))
 print("train_label shape=" + str(np.shape(train_labels_OHE)))
 
@@ -44,59 +48,60 @@ for rowIdx in range(numTestRows):
 print("test_data shape=" + str(np.shape(test_data)))
 print("test_label shape=" + str(np.shape(test_labels_OHE)))
 
-LEARNING_RATE = 0.3
 NUM_INPUT_NODES = numTrainCols
 NUM_OUTPUT_NODES = numUniqueOutputs
 
-NUM_HIDDEN_NODES = [28]
-print(f"Num Inputs:\t{NUM_INPUT_NODES}")
-print(f"Num Hidden:\t{NUM_HIDDEN_NODES}")
-print(f"Num Outputs:\t{NUM_OUTPUT_NODES}")
-neuralNetwork = NeuralNetwork(
-    NUM_INPUT_NODES, NUM_OUTPUT_NODES, LEARNING_RATE, *NUM_HIDDEN_NODES
-)  
+learningRates = [base / 10 ** exponent for exponent in range(1, 3) for base in range(9, 0, -1)]
 
-PASS_CRITERIA = 0.9
+def getWidgets(message):
+    return [
+        f" {message}: [",
+        progressbar.Percentage(),
+        "]",
+        progressbar.Bar(),
+        " (",
+        progressbar.Timer(),
+        ") ",
+    ]
 
-outputsNN = np.zeros(test_labels_OHE.shape, dtype=float)
+fields = ["No. Hidden Nodes"] + [str(learningRate) for learningRate in learningRates]
+with open(destinationFileName, "w", newline='') as outputCSV:
+    writer = csv.DictWriter(outputCSV, fieldnames=fields)
+    writer.writeheader()
 
-widgets = [
-    " TRAINING: [",
-    progressbar.Percentage(),
-    "]",
-    progressbar.Bar(),
-    " (",
-    progressbar.Timer(),
-    ") ",
-]
-for rowIdx in progressbar.progressbar(
-    range(numTrainRows), redirect_stdout=True, widgets=widgets
-):
-    row = train_data[rowIdx]
-    targets = train_labels_OHE[rowIdx]
-    neuralNetwork.train(row, targets)
+for numHiddenNodes in range(900, 1000, 50):
+    results = {"No. Hidden Nodes": numHiddenNodes}
+    for learningRateIdx in progressbar.progressbar(
+        range(len(learningRates)), redirect_stdout=True, widgets=getWidgets(f"Hidden Nodes: {numHiddenNodes}")
+        ):
+        learningRate = learningRates[learningRateIdx]
+        neuralNetwork = NeuralNetwork(
+            NUM_INPUT_NODES, NUM_OUTPUT_NODES, learningRate, numHiddenNodes
+        )
 
-widgets = [
-    " TESTING:  [",
-    progressbar.Percentage(),
-    "]",
-    progressbar.Bar(),
-    " (",
-    progressbar.Timer(),
-    ") ",
-]
-for rowIdx in progressbar.progressbar(
-    range(numTestRows), redirect_stdout=True, widgets=widgets
-):
-    row = test_data[rowIdx]
-    output = neuralNetwork.query(row)
-    outputNN = outputsNN[rowIdx]
-    outputsNN[rowIdx] = np.reshape(output, (numUniqueOutputs))
+        outputsNN = np.zeros(test_labels_OHE.shape, dtype=float)
 
-outputsNN = outputsNN * test_labels_OHE
-numPassed = (outputsNN >= PASS_CRITERIA).sum()
+        for rowIdx in range(numTrainRows):
+            row = train_data[rowIdx]
+            targets = train_labels_OHE[rowIdx]
+            neuralNetwork.train(row, targets)
 
-print(f"Passed:\t{numPassed}/{numTestRows}")
+        for rowIdx in range(numTestRows):
+            row = test_data[rowIdx]
+            output = neuralNetwork.query(row)
+            outputNN = outputsNN[rowIdx]
+            outputsNN[rowIdx] = np.reshape(output, (numUniqueOutputs))
 
-# neuralNetwork.trainUntilPass(df_train, df_test, maxIterations=MAX_ITERATIONS, minPrecision=PRECISION)
-# neuralNetwork.showStateSpaceRepresentation(inputs, targets)
+        maximumValues = np.amax(outputsNN, axis=1)
+        maximumValues = np.tile(maximumValues, (numUniqueOutputs, 1))
+        outputsNN = np.floor(outputsNN / maximumValues.T)
+        outputsNN = outputsNN * test_labels_OHE
+        numPassed = outputsNN.sum()
+
+        print(f"Hidden Nodes:\t{numHiddenNodes}\tLearning Rate:\t{learningRate}\tPassed:\t{(numPassed/numTestRows)*100}%")
+        results[str(learningRate)] = numPassed
+    
+    with open(destinationFileName, "a", newline='') as outputCSV:
+        writer = csv.DictWriter(outputCSV, fieldnames=fields)
+        writer.writerow(results)
+
